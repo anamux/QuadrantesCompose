9c08c11c9b0506a607ede185b65a3ced3017daf2
anamux <anaamux@gmail.com>
2023-11-10 15:29:37 -0300

    implementação da apresentação dos textos em 4 quadrantes.

--- app/src/main/java/br/com/ana/quadrantescompose/MainActivity.kt
+++ app/src/main/java/br/com/ana/quadrantescompose/MainActivity.kt
@@ -3,14 +3,28 @@ package br.com.ana.quadrantescompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.ana.quadrantescompose.ui.theme.QuadrantesComposeTheme
+import java.nio.file.WatchEvent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -30,11 +44,66 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Greeting() {
+    Column(Modifier.fillMaxSize()) {
+        Row (Modifier.weight(1f)) {
+            GreetingCard(
+                title = stringResource(R.string.title_1),
+                description = stringResource(R.string.description_1),
+                background = colorResource(R.color.color_1),
+                modifier = Modifier.weight(1f)
+                )
+            GreetingCard(
+                title = stringResource(R.string.title_2),
+                description = stringResource(R.string.description_2),
+                background = colorResource(R.color.color_2),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row (Modifier.weight(1f)) {
+            GreetingCard(
+                title = stringResource(R.string.title_3),
+                description = stringResource(R.string.description_3),
+                background = colorResource(R.color.color_3),
+                modifier = Modifier.weight(1f)
+            )
+            GreetingCard(
+                title = stringResource(R.string.title_4),
+                description = stringResource(R.string.description_4),
+                background = colorResource(R.color.color_4),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+fun GreetingCard(
+    title: String,
+    description: String,
+    background: Color,
+    modifier: Modifier = Modifier,
+) {
+    Column(
         modifier = modifier
+            .fillMaxSize()
+            .background(background)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     )
+    {
+        Text(
+            text = title,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify
+        )
+    }
+
 }
 
 @Preview(
@@ -44,6 +113,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     QuadrantesComposeTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
